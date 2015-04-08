4d38fda80714e7da23d0b19c15664148c28fd1f8
bruna_ss1 <bruna_ss1@estudante.sc.senai.br>
2015-04-08 20:21:11 -0300

    Modifiquei teu projeto -- Syso Bruna aqui!

--- ExercicioGit1/src/EstarBem.java
+++ ExercicioGit1/src/EstarBem.java
@@ -2,5 +2,6 @@
 public class EstarBem {
  public static void main(String[] args) {
 	System.out.println("Git Git Git Git Git Git " + "Oi Oi Oi Oi Oi Oi Oi");
+	System.out.println("Bruna aqui!");
 }
 }
